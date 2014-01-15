a37704495bed46c01bb660b2c41d53532ad911f6
setsumi <kreliahn@gmail.com>
2014-01-14 21:11:36 -0800

    New anura build. Raw water. Now set test-level.cfg for experiments.

--- anura-stuff/anura-diff/metroiduh.cmd
+++ anura-stuff/anura-diff/metroiduh.cmd
@@ -1,8 +1,8 @@
 setlocal
 set LANG=en
 
-anura.exe --module=metroiduh --no-send-stats --no-tests
-::anura.exe --module=metroiduh --level=tutorial.cfg --no-send-stats --no-tests
+::anura.exe --module=metroiduh --no-send-stats --no-tests
+anura.exe --module=metroiduh --level=test-level.cfg --no-send-stats --no-tests
 
 ::pause
 
