92381450d439020341774ecc39ed5af45b558f44
setsumi <kreliahn@gmail.com>
2014-01-14 21:18:46 -0800

    Small fix of run game shell scripts.

---
+++ anura-stuff/anura-diff/anura-test-level.cmd
@@ -0,0 +1,6 @@
+setlocal
+set LANG=en
+
+anura.exe --module=metroiduh --level=test-level.cfg --no-send-stats --no-tests
+
+::pause
--- anura-stuff/anura-diff/metroiduh.cmd
+++ anura-stuff/anura-diff/metroiduh.cmd
@@ -1,8 +1,7 @@
 setlocal
 set LANG=en
 
-::anura.exe --module=metroiduh --no-send-stats --no-tests
-anura.exe --module=metroiduh --level=test-level.cfg --no-send-stats --no-tests
+anura.exe --module=metroiduh --no-send-stats --no-tests
 
 ::pause
 
